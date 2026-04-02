fa42337c7df157190b90d38a6ef12d078bc58f15
maximoskp <maximoskalpap@gmail.com>
2026-04-02 14:59:39 +0000

    generated from checkpoints

--- generate_ED_pretrain.py
+++ generate_ED_pretrain.py
@@ -25,21 +25,6 @@ else:
     else:
         print('Selected device not available: ' + device_name)
 # end device selection
-d_model = 512
-transformer_model = EDFiLMModel(
-    chord_vocab_size=len(tokenizer.vocab),
-    d_model=d_model,
-    nhead=4,
-    num_layers=4,
-    grid_length=80,
-    pianoroll_dim=tokenizer.pianoroll_dim,
-    guidance_dim=d_model,
-    device=device,
-)
-checkpoint = torch.load(f'saved_models/ED/pretrained.pt', map_location=device_name)
-transformer_model.load_state_dict(checkpoint)
-transformer_model.to(device)
-transformer_model.eval()
 
 jazz_path = '/mnt/ssd2/maximos/data/gjt_melodies/gjt_CA_test'
 jazz_files = [f for f in os.listdir(jazz_path) if f.endswith('.mxl') or f.endswith('.xml')]
@@ -52,34 +37,52 @@ all_files = [jazz_files, nottingham_files]
 all_names = ['jazz', 'nottingham']
 
 loss_scheme = 'pre_none'
-for i in [0,1]:
-    data_path = all_paths[i]
-    data_files = all_files[i]
-    data_name = all_names[i]
-    for i in tqdm(range(len(data_files))):
-        h_idx = i
-        input_f_path = os.path.join(data_path, data_files[h_idx])
-        mxl_folder_out = None
-        prefix = 'gen/ED/'
-        midi_folder_out = f'MIDIs/{data_name}/{prefix}{loss_scheme}'
-        name_suffix = str(h_idx)
-        
-        g = generate_files_with_nucleus(
-            transformer_model,
-            tokenizer,
-            input_f_path,
-            mxl_folder_out,
-            midi_folder_out,
-            name_suffix,
-            guidance_f_path = None,
-            guidance_vec = None,
-            use_constraints=False,
-            intertwine_bar_info=True, # no bar default
-            normalize_tonality=False,
-            temperature=0.5,
-            p=0.9,
-            unmasking_order='start',
-            create_gen = True,
-            create_real = False,
-            create_guide = False
-        )
+model_names = os.listdir('saved_models/ED/')
+for name in model_names:
+    print('ED - Model name: ' + name)
+    d_model = 512
+    transformer_model = EDFiLMModel(
+        chord_vocab_size=len(tokenizer.vocab),
+        d_model=d_model,
+        nhead=4,
+        num_layers=4,
+        grid_length=80,
+        pianoroll_dim=tokenizer.pianoroll_dim,
+        guidance_dim=d_model,
+        device=device,
+    )
+    checkpoint = torch.load(f'saved_models/ED/{name}', map_location=device_name)
+    transformer_model.load_state_dict(checkpoint)
+    transformer_model.to(device)
+    transformer_model.eval()
+    for i in [0,1]:
+        data_path = all_paths[i]
+        data_files = all_files[i]
+        data_name = all_names[i]
+        for i in tqdm(range(len(data_files))):
+            h_idx = i
+            input_f_path = os.path.join(data_path, data_files[h_idx])
+            mxl_folder_out = None
+            prefix = f'gen/ED/{name.split(".")[0]}/'
+            midi_folder_out = f'MIDIs/{data_name}/{prefix}{loss_scheme}'
+            name_suffix = str(h_idx)
+            
+            g = generate_files_with_nucleus(
+                transformer_model,
+                tokenizer,
+                input_f_path,
+                mxl_folder_out,
+                midi_folder_out,
+                name_suffix,
+                guidance_f_path = None,
+                guidance_vec = None,
+                use_constraints=False,
+                intertwine_bar_info=True, # no bar default
+                normalize_tonality=False,
+                temperature=0.5,
+                p=0.9,
+                unmasking_order='start',
+                create_gen = True,
+                create_real = False,
+                create_guide = False
+            )
--- generate_SE_pretrained.py
+++ generate_SE_pretrained.py
@@ -25,21 +25,6 @@ else:
     else:
         print('Selected device not available: ' + device_name)
 # end device selection
-d_model = 512
-transformer_model = SEFiLMModel(
-    chord_vocab_size=len(tokenizer.vocab),
-    d_model=d_model,
-    nhead=8,
-    num_layers=8,
-    grid_length=80,
-    pianoroll_dim=tokenizer.pianoroll_dim,
-    guidance_dim=d_model,
-    device=device,
-)
-checkpoint = torch.load(f'saved_models/SE/pretrained.pt', map_location=device_name)
-transformer_model.load_state_dict(checkpoint)
-transformer_model.to(device)
-transformer_model.eval()
 
 jazz_path = '/mnt/ssd2/maximos/data/gjt_melodies/gjt_CA_test'
 jazz_files = [f for f in os.listdir(jazz_path) if f.endswith('.mxl') or f.endswith('.xml')]
@@ -52,34 +37,52 @@ all_files = [jazz_files, nottingham_files]
 all_names = ['jazz', 'nottingham']
 
 loss_scheme = 'pre_none'
-for i in [0,1]:
-    data_path = all_paths[i]
-    data_files = all_files[i]
-    data_name = all_names[i]
-    for i in tqdm(range(len(data_files))):
-        h_idx = i
-        input_f_path = os.path.join(data_path, data_files[h_idx])
-        mxl_folder_out = None
-        prefix = 'gen/SE/'
-        midi_folder_out = f'MIDIs/{data_name}/{prefix}{loss_scheme}'
-        name_suffix = str(h_idx)
-        
-        g = generate_files_with_nucleus(
-            transformer_model,
-            tokenizer,
-            input_f_path,
-            mxl_folder_out,
-            midi_folder_out,
-            name_suffix,
-            guidance_f_path = None,
-            guidance_vec = None,
-            use_constraints=False,
-            intertwine_bar_info=True, # no bar default
-            normalize_tonality=False,
-            temperature=0.5,
-            p=0.9,
-            unmasking_order='certain',
-            create_gen = True,
-            create_real = False,
-            create_guide = False
-        )
+model_names = os.listdir('saved_models/SE/')
+for name in model_names:
+    print('SE - Model name: ' + name)
+    d_model = 512
+    transformer_model = SEFiLMModel(
+        chord_vocab_size=len(tokenizer.vocab),
+        d_model=d_model,
+        nhead=8,
+        num_layers=8,
+        grid_length=80,
+        pianoroll_dim=tokenizer.pianoroll_dim,
+        guidance_dim=d_model,
+        device=device,
+    )
+    checkpoint = torch.load(f'saved_models/SE/{name}', map_location=device_name)
+    transformer_model.load_state_dict(checkpoint)
+    transformer_model.to(device)
+    transformer_model.eval()
+    for i in [0,1]:
+        data_path = all_paths[i]
+        data_files = all_files[i]
+        data_name = all_names[i]
+        for i in tqdm(range(len(data_files))):
+            h_idx = i
+            input_f_path = os.path.join(data_path, data_files[h_idx])
+            mxl_folder_out = None
+            prefix = f'gen/SE/{name.split(".")[0]}/'
+            midi_folder_out = f'MIDIs/{data_name}/{prefix}{loss_scheme}'
+            name_suffix = str(h_idx)
+            
+            g = generate_files_with_nucleus(
+                transformer_model,
+                tokenizer,
+                input_f_path,
+                mxl_folder_out,
+                midi_folder_out,
+                name_suffix,
+                guidance_f_path = None,
+                guidance_vec = None,
+                use_constraints=False,
+                intertwine_bar_info=True, # no bar default
+                normalize_tonality=False,
+                temperature=0.5,
+                p=0.9,
+                unmasking_order='certain',
+                create_gen = True,
+                create_real = False,
+                create_guide = False
+            )
