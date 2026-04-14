8f27067e03b1164426cbc13fc0806f45c77ec4f6
maximoskp <maximoskalpap@gmail.com>
2026-04-14 17:32:12 +0000

    housekeeping

---
+++ generate_pbp_SEAS_gjt2nott.py
@@ -0,0 +1,150 @@
+from generate_utils import generate_files_with_nucleus, load_SEASModel, get_actisteer_guidance
+import GridMLM_tokenizers
+from GridMLM_tokenizers import CSGridMLMTokenizer
+import os
+from tqdm import tqdm
+import pickle
+import torch
+import numpy as np
+from old.run_midi_metrics_source_target import compute_all_metrics, has_non_positive, source_target_distances
+import pandas as pd
+import numpy as np
+
+num_guides_per_piece = 5
+results_base_path = 'results/SE/pbp/ActivationSteering'
+os.makedirs(results_base_path, exist_ok=True)
+
+device_name = 'cuda:0'
+
+tokenizer = CSGridMLMTokenizer(
+    fixed_length=80,
+    quantization='4th',
+    intertwine_bar_info=True,
+    trim_start=False,
+    use_pc_roll=True,
+    use_full_range_melody=False
+)
+
+source_path = 'MIDIs/jazz/real'
+source_files = [f for f in os.listdir(source_path) if f.endswith('.mid') or f.endswith('.midi')]
+
+target_path = 'MIDIs/nottingham/real'
+target_files = [f for f in os.listdir(target_path) if f.endswith('.mid') or f.endswith('.midi')]
+
+np.random.seed(42)
+guide_idxs = {}
+for i in range(len(source_files)):
+    guide_idxs[i] = np.random.permutation(len(target_files))[:num_guides_per_piece]
+
+loss_scheme = 'l'
+model = load_SEASModel(
+    tokenizer,
+    device_name,
+    d_model=512
+)
+
+results_bin_all = {}
+results_dist_all = {}
+
+for layers_to_steer in [ [4,5,6], [5,6,7], [3,4,5,6,7], [0,1,2,3,4,5,6,7] ]:
+    for alpha in [0.5, 1.0, 2.5, 5.0, 7.0, 10.0, 20.0]:
+        folder_name = 'layers_' + \
+            str(layers_to_steer).replace(', ','_').replace('[', '').replace(']', '') + \
+            '_alpha_' + str(alpha).replace('.', '_')
+        print(folder_name)
+        num_gen = 0
+        bin_all = {}
+        dist_all = {}
+        total_examples = len(source_files)*num_guides_per_piece
+        for h_idx in range(len(source_files)):
+            for g_idx in guide_idxs[h_idx]:
+                print(f'folder_name: {folder_name} - trying: {num_gen} / {total_examples}')
+                # home file
+                input_f_path = os.path.join(source_path, source_files[h_idx])
+                # guide file
+                guide_f_path = os.path.join(target_path, target_files[g_idx])
+                mxl_folder_out = None
+                prefix = 'gen/SE/'
+                midi_folder_out = f'MIDIs/jazz2nott_pbp/{prefix}{folder_name}'
+                name_suffix = (loss_scheme == 'real' or loss_scheme == 'none')*str(h_idx) + \
+                    (loss_scheme != 'real' and loss_scheme != 'none')*f'h{h_idx}_g{g_idx}'
+                
+                # encode input to get reference point
+                source_encoded = tokenizer.encode(input_f_path)
+                # encode guide to get reference point
+                target_encoded = tokenizer.encode(guide_f_path)
+                h = {}
+                tmp_h = get_actisteer_guidance(
+                    model,
+                    tokenizer.bar_token_id,
+                    tokenizer.mask_token_id,
+                    torch.tensor(source_encoded['pianoroll']).unsqueeze(0).to(model.device),
+                    torch.tensor(source_encoded['harmony_ids']).unsqueeze(0).to(model.device),
+                    torch.tensor(target_encoded['pianoroll']).unsqueeze(0).to(model.device),
+                    torch.tensor(target_encoded['harmony_ids']).unsqueeze(0).to(model.device),
+                )
+                for ii in layers_to_steer:
+                    h[ii] = tmp_h[ii]
+
+                g = generate_files_with_nucleus(
+                    model,
+                    tokenizer,
+                    input_f_path,
+                    mxl_folder_out,
+                    midi_folder_out,
+                    name_suffix,
+                    guidance_f_path=None,
+                    guidance_vec=None,
+                    steering_vec=h,
+                    steering_alpha=alpha,
+                    use_constraints=False,
+                    intertwine_bar_info=True, # no bar default
+                    normalize_tonality=False,
+                    temperature=1.0,
+                    p=0.9,
+                    unmasking_order='certain',
+                    create_gen = loss_scheme != 'real',
+                    create_real = loss_scheme == 'real',
+                    create_guide = False
+                )
+                # midi out path
+                midi_out_path = os.path.join(midi_folder_out, 'gen_' + name_suffix + '.mid')
+
+                # compute metrics
+                source_metrics = compute_all_metrics(input_f_path)
+                target_metrics = compute_all_metrics(guide_f_path)
+                gen_metrics = compute_all_metrics(midi_out_path)
+
+                # CHE CC CTD CTnCTR PCS MCTD HRHE HRC CBS
+                num_gen += 1
+                bin_out, dist_diff = source_target_distances(gen_metrics, source_metrics, target_metrics)
+                for k, v in bin_out.items():
+                    if k in bin_all.keys():
+                        bin_all[k] += bin_out[k]
+                        dist_all[k] += dist_diff[k]
+                    else:
+                        bin_all[k] = bin_out[k]
+                        dist_all[k] = dist_diff[k]
+                # end for items
+            # end for g_idx counter
+        # end for h_idx counter
+    # make average metrics
+    results_bin_all[folder_name] = {}
+    results_dist_all[folder_name] = {}
+    for k in bin_all.keys():
+        results_bin_all[folder_name][k] = bin_all[k]/total_examples
+        results_dist_all[folder_name][k] = dist_all[k]/total_examples
+    # end for loss_schemes
+# save to csvs
+df_bin = pd.DataFrame.from_dict(results_bin_all, orient='index')
+df_dist = pd.DataFrame.from_dict(results_dist_all, orient='index')
+
+df_bin.to_csv(os.path.join(results_base_path, 'gjt2_nott_bin.csv'))
+df_dist.to_csv(os.path.join(results_base_path, 'gjt2_nott_dist.csv'))
+
+latex_table = df_bin.to_latex(float_format="%.4f")
+with open(os.path.join(results_base_path, 'gjt2_nott_bin.tex'), "w") as f:
+    f.write(latex_table)
+latex_table = df_dist.to_latex(float_format="%.4f")
+with open(os.path.join(results_base_path, 'gjt2_nott_dist.tex'), "w") as f:
+    f.write(latex_table)
---
+++ generate_pretrained_SE.py
@@ -0,0 +1,88 @@
+from generate_utils import generate_files_with_nucleus
+import GridMLM_tokenizers
+from GridMLM_tokenizers import CSGridMLMTokenizer
+import os
+from tqdm import tqdm
+import torch
+from models import SEFiLMModel
+
+device_name = 'cuda:0'
+
+tokenizer = CSGridMLMTokenizer(
+    fixed_length=80,
+    quantization='4th',
+    intertwine_bar_info=True,
+    trim_start=False,
+    use_pc_roll=True,
+    use_full_range_melody=False
+)
+
+if device_name == 'cpu':
+    device = torch.device('cpu')
+else:
+    if torch.cuda.is_available():
+        device = torch.device(device_name)
+    else:
+        print('Selected device not available: ' + device_name)
+# end device selection
+
+jazz_path = '/mnt/ssd2/maximos/data/gjt_melodies/gjt_CA_test'
+jazz_files = [f for f in os.listdir(jazz_path) if f.endswith('.mxl') or f.endswith('.xml')]
+
+nottingham_path = '/mnt/ssd2/maximos/data/mel_harm_other_CA/nottingham_test'
+nottingham_files = [f for f in os.listdir(nottingham_path) if f.endswith('.mid') or f.endswith('.midi')]
+
+all_paths = [jazz_path, nottingham_path]
+all_files = [jazz_files, nottingham_files]
+all_names = ['jazz', 'nottingham']
+
+loss_scheme = 'pre_none'
+model_names = os.listdir('saved_models/SE/')
+for name in model_names:
+    print('SE - Model name: ' + name)
+    d_model = 512
+    transformer_model = SEFiLMModel(
+        chord_vocab_size=len(tokenizer.vocab),
+        d_model=d_model,
+        nhead=8,
+        num_layers=8,
+        grid_length=80,
+        pianoroll_dim=tokenizer.pianoroll_dim,
+        guidance_dim=d_model,
+        device=device,
+    )
+    checkpoint = torch.load(f'saved_models/SE/{name}', map_location=device_name)
+    transformer_model.load_state_dict(checkpoint)
+    transformer_model.to(device)
+    transformer_model.eval()
+    for i in [0,1]:
+        data_path = all_paths[i]
+        data_files = all_files[i]
+        data_name = all_names[i]
+        for i in tqdm(range(len(data_files))):
+            h_idx = i
+            input_f_path = os.path.join(data_path, data_files[h_idx])
+            mxl_folder_out = None
+            prefix = f'gen/SE/{name.split(".")[0]}/'
+            midi_folder_out = f'MIDIs/{data_name}/{prefix}'
+            name_suffix = str(h_idx)
+            
+            g = generate_files_with_nucleus(
+                transformer_model,
+                tokenizer,
+                input_f_path,
+                mxl_folder_out,
+                midi_folder_out,
+                name_suffix,
+                guidance_f_path = None,
+                guidance_vec = None,
+                use_constraints=False,
+                intertwine_bar_info=True, # no bar default
+                normalize_tonality=False,
+                temperature=1.0,
+                p=0.9,
+                unmasking_order='certain',
+                create_gen = True,
+                create_real = False,
+                create_guide = False
+            )
